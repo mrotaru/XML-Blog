e5463a00fa770811931dc15feeddd8aab24266f4
Mihai Rotaru <mihai_rotaru@lavabit.com>
2012-03-29 11:59:38 +0100

    add JQuery, and tooltip plugin

---
+++ script.js
@@ -0,0 +1,113 @@
+var xml_posts;
+
+function loadXMLDoc(docname)
+{   
+    if (window.XMLHttpRequest)
+    {
+        xhttp=new XMLHttpRequest();
+    }
+
+    xhttp.open("GET",docname,false);
+    xhttp.send("");
+    return xhttp.responseXML;
+}
+
+function loadXMLString(txt)
+{
+    if (window.DOMParser)
+    {
+        parser=new DOMParser();
+        xmlDoc=parser.parseFromString(txt,"text/xml");
+    }
+    else // Internet Explorer
+    {
+        xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async="false";
+        xmlDoc.loadXML(txt);
+    }
+
+    return xmlDoc;
+}
+
+// returns all methods supported by obj
+// from: http://stackoverflow.com/questions/152483/is-there-a-way-to-print-all-methods-of-an-object-in-javascript 
+function getMethods(obj) {
+    var result = [];
+    for( var id in obj ) {
+        try {
+            if( typeof( obj[id] ) == "function" ) {
+                result.push( id + ": " + obj[id].toString() );
+            }
+        } catch( err ) {
+            result.push( id + ": inaccessible" );
+        }
+    }
+    return result;
+}
+
+// check if the next sibling node is an element node
+// ---------------------------------------------------------------
+// Node types:
+// 1 	ELEMENT_NODE
+// 2 	ATTRIBUTE_NODE
+// 3 	TEXT_NODE
+// 4 	CDATA_SECTION_NODE
+// 5 	ENTITY_REFERENCE_NODE
+// 6 	ENTITY_NODE
+// 7 	PROCESSING_INSTRUCTION_NODE
+// 8 	COMMENT_NODE
+// 9 	DOCUMENT_NODE
+// 10 	DOCUMENT_TYPE_NODE
+// 11 	DOCUMENT_FRAGMENT_NODE
+// 12 	NOTATION_NODE
+// ---------------------------------------------------------------
+function get_nextsibling( node )
+{
+    x = node.nextSibling;
+    while( x && x.nodeType != 1 )
+    {
+        x = x.nextSibling;
+    }
+    return x;
+}
+
+// check if the first node is an element node
+function get_firstchild( node )
+{
+    x = node.firstChild;
+    while( x && x.nodeType !=1 )
+    {
+        x = x.nextSibling;
+    }
+    return x;
+}
+
+function get_tooltip_html( id )
+{
+    // load xml file
+    // extract info
+    // return
+}
+
+function generate_tooltips()
+{
+    console.info("generating tooltips");
+    $('#post_00_anchor').tooltip({
+        bodyHandler: function() {
+            return '<h3>' + $(this).attr("my_title") + '</h3>' +
+            '<p>A very interesting blog post</p>';
+        },
+        track: true,
+        delay: 0,
+        showURL: false,
+        showBody: " - ",
+        fade: 150
+    });
+}
+
+function init()
+{
+    $("#content").html('<a href="http://www.google.co.uk" title="This is a tooltit which works!">This was added with JQuery!</a>');
+    xml_posts=loadXMLDoc("posts_with_titles.xml");
+    generate_tooltips();
+}
--- style.xsl
+++ style.xsl
@@ -6,6 +6,14 @@
     <html>
         <head>
             <link rel="stylesheet" href="style.css"/>
+            <script src="lib/jquery.js" type="text/javascript"></script>
+            <script src="lib/jquery.bgiframe.js" type="text/javascript"></script>
+            <script src="lib/jquery.dimensions.js" type="text/javascript"></script>
+            <script src="lib/jquery.tooltip.pack.js" type="text/javascript"></script>
+            <script src="script.js" type="text/javascript"></script>
+            <script>
+                window.onload=init(); 
+            </script>
             <title>
                 <xsl:value-of select="$title"/>
             </title>
@@ -22,12 +30,27 @@
                     <ul>
                         <xsl:for-each select="document(/root/@posts)/posts/post">
                             <xsl:variable name="post_title" select="@title"/>
-                            <xsl:variable name="filename" select="@filename"/>
+                            <xsl:variable name="filename" select="substring-after(@filename,'/')"/>
+
+                            <!-- JQuery apparently has issues with id's containing periods and commas, so remove them.-->
+                            <!-- The generated anchors will have id's of the form: -->
+                            <!-- <a id="post_01_anchor"> -->
+                                <xsl:variable name="id" select="concat(
+                                                                    translate( 
+                                                                        substring($filename, 0, string-length($filename)-3), '.,','_'),
+                                                                    '_anchor'
+                                                                    )"/>
                             <li>
                                 <a>
                                     <xsl:attribute name="id">
-                                        <xsl:value-of select="$filename"/>
+                                        <xsl:value-of select="$id"/>
                                     </xsl:attribute>
+
+                                    <!-- adding a tooltip removes the 'title' attribute, so use a different name-->
+                                    <xsl:attribute name="my_title">
+                                        <xsl:value-of select="$post_title"/>
+                                    </xsl:attribute>
+
                                     <xsl:attribute name="href">
                                         <xsl:value-of select="$filename"/>
                                     </xsl:attribute>
